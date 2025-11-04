4007595e144aed625099ffb9853d3dd59302b42c
Aidan Baydush <amb314@duke.edu>
2025-11-04 16:13:10 -0500

    added method headers for dino file

---
+++ dino.py
@@ -0,0 +1,41 @@
+# @aidan-baydush [Aidan Baydush] @ ... input name bowen
+
+
+import numpy as np
+import pandas as pd
+import random
+import math
+from typing import Iterator
+
+
+
+# some function headers
+def set_deck() -> Deck:
+...
+return
+
+
+def get_tip_generator(deck: Deck) -> Iterator
+...
+return
+
+
+def push_dino_board(lh: LiquidHandler, plate: Plate):
+...
+return
+
+
+def dino_jump(lh: LiquidHandler, plate: Plate):
+...
+return
+
+
+def dino_squat(lh: LiquidHandler, plate: Plate):
+...
+
+
+def dino_return(lh: LiquidHandler, plate: Plate):
+...
+
+
+
